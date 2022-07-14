e113a528f27540aaa4ec7f7ee3bf232ec1d477fa
Chainyeop <thunder2756@naver.com>
2022-07-14 15:40:04 +0900

    first commit

--- 1427_22_07_14.cpp
+++ 1427_22_07_14.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
+#include<algorithm>
+#include<string>
 using namespace std;
 int main(){
-    
+    string b;
+    cin>>b;
+    sort(b.begin(),b.end(),greater<int>());
+    cout<<b;
 }
